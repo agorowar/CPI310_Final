ad485f272b6f943ca668d9bdfad10831cd3de4c7
agorowar <42878457+agorowar@users.noreply.github.com>
2019-11-08 17:55:57 -0700

    Implemented logout function

--- Website/index.js
+++ Website/index.js
@@ -47,6 +47,17 @@ app.get("/", async (req, res)=>{
     res.render("index", {user: req.user });
 });
 
+//logout functionality
+app.post("/", async(req,res)=>{
+    const db =await dbPromise;
+    const token = req.cookies.authToken;
+    const authToken = await db.get("SELECT * FROM authToken WHERE token=?", token)
+    const user = await db.get("SELECT * FROM users WHERE id=?", authToken.userId)
+    req.user = user;
+    await db.run("DELETE FROM authToken WHERE token=?", token)
+    return res.render("index", {error: "user logged out"});
+});
+
 app.get("/login", (req,res)=>{
     res.render("login");
 });
